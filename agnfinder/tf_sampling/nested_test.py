a8cbab2ec77097500be055f92e6c3f3d0ab20bf4
Mike Walmsley <walmsleymk1@gmail.com>
2019-10-01 02:24:16 -0700

    Abandon for now as too fiddle

--- agnfinder/tf_sampling/nested_test.py
+++ agnfinder/tf_sampling/nested_test.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
-from nestorflow.nested import nested_sample
+from nestorflow.nested import nested_sample, samples
 import matplotlib.pyplot as plt
+import os
 
 from agnfinder.tf_sampling.api import get_log_prob_fn
 
@@ -13,10 +14,13 @@ from agnfinder.tf_sampling.api import get_log_prob_fn
 
 if __name__ == '__main__':
 
+    os.environ['TF_XLA_FLAGS']='--tf_xla_cpu_global_jit'
+
     n_dims = 1  # params or observables?
     n_live = 10
-    n_repeats = n_dims*2
-    n_compress = n_dims*5
+    # n_repeats = n_dims*2
+    n_repeats = 1
+    # n_compress = n_dims*5
 
     # true_observation = np.array([0.5] * 5).astype(np.float32)
     true_observation = 0.5
@@ -29,18 +33,28 @@ if __name__ == '__main__':
     # def likelihood(x):
         # return tf.expand_dims(tf.log(tf.abs(forward_model(x, training=False) - true_observation)), axis=0)
 
-    @tf.function
+    # @tf.function
     def likelihood(x):
         return tf.squeeze(tf.log(tf.abs(forward_model(x, training=False) - true_observation)))
 
     # likelihood = tf.function(get_log_prob_fn(forward_model, true_observation, batch_dim=None))
 
     # points, L = nested_sample(likelihood, n_live, n_dims, n_repeats, n_compress)
-    samples, likelihoods = nested_sample(likelihood, n_live, n_dims, n_repeats)
+    points, L = nested_sample(likelihood, n_live, n_dims, n_repeats)
+    posterior, logw = samples(points, L)
 
     sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-    likelihoods, samples = sess.run([likelihoods, samples])
+    sess.run(tf.compat.v1.global_variables_initializer())
+    # likelihoods, samples = sess.run([likelihoods, samples])
+    posterior, logw = sess.run([posterior, logw])
+
+    print(posterior)
+    print(posterior.shape)
+    exit()
     
-    print(samples)
+    # print(samples)
+    # print(samples.shape)
+    # plt.hist(samples.flatten())
+    # plt.tight_layout()
+    # plt.savefig('temp_nested_samples.png')
     # print(samples.numpy())  # should all be spread near 0.5
