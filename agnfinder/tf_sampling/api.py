Improvements to sampler and diagnostics - enforces bounds in log_prob - minus sign fixed - diagnostics from arviz - nans removed
Now works for most parameters with random starts
Diagnostics show chains not yet converged

TODO: get the optimised starts working, rather than merely not failing

agnfinder/tf_sampling/api.py
@@ -1,5 +1,6 @@
 import logging
 
+import numpy as np
 import tensorflow as tf
 
 class SamplingProblem():
@@ -27,17 +28,25 @@ class Sampler():
         return self.sample()
 
 
-# TODO refactor
+# @tf.function  # inputs become tensors when you wrap
 def get_log_prob_fn(forward_model, true_observation, batch_dim=None):
     if batch_dim is not None:
-        true_observation_stacked = tf.stack([tf.constant(true_observation) for n in range(batch_dim)])
+        true_observation_stacked = tf.stack([true_observation for n in range(batch_dim)], axis=0)
     else:
         true_observation_stacked = tf.reshape(true_observation, (1, -1))
     # first dimension of true params must match first dimension of x, or will fail
-    def log_prob_fn(x):
+    @tf.function
+    def log_prob_fn(x):  # 0th axis is batch/chain dim, 1st is param dim
         expected_photometry = forward_model(x, training=False)  # model expects a batch dimension, which here is the chains
         deviation = tf.abs(10 ** expected_photometry - 10 ** true_observation_stacked)
         sigma = (10 ** expected_photometry) * 0.05
-        log_prob = -tf.reduce_sum(deviation / sigma, axis=1)
+        log_prob = -tf.reduce_sum(deviation / sigma, axis=1)  # very negative = unlikely, near -0 = likely
+        x_out_of_bounds = is_out_of_bounds(x)
+        penalty = tf.cast(x_out_of_bounds, tf.float32) * tf.constant(1000., dtype=tf.float32)
+        log_prob_with_penalty = log_prob - penalty  # no effect if x in bounds, else divide (subtract) a big penalty
         return log_prob
     return log_prob_fn
+
+
+def is_out_of_bounds(x):  # x expected to have (batch, param) shape
+    return tf.reduce_any( tf.cast(x > 1., tf.bool) | tf.cast(x < 0., tf.bool),  axis=1)

agnfinder/tf_sampling/hmc.py
@@ -21,7 +21,6 @@ class SamplerHMC(Sampler):
 
     def sample(self):
         log_prob_fn = get_log_prob_fn(self.problem.forward_model, self.problem.true_observation, batch_dim=self.n_chains)
-
         if self.init_method == 'correct':
             true_params_stacked = np.vstack([self.problem.true_params.astype(np.float32) for n in range(self.n_chains)])
             initial_state = tf.constant(true_params_stacked)
@@ -29,12 +28,14 @@ class SamplerHMC(Sampler):
             not_quite_true_params = np.vstack([self.problem.true_params for n in range(self.n_chains)] + np.random.rand(self.n_chains, len(self.problem.true_params)) * 0.03).astype(np.float32)
             initial_state = tf.constant(not_quite_true_params)
         elif self.init_method == 'random':
-            initial_state = many_random_starts(self.problem.forward_model, self.problem.true_observation, self.problem.true_params, self.n_chains)
+            initial_state = many_random_starts(self.problem.forward_model, self.problem.true_observation, self.problem.n_dim, self.n_chains)
         elif self.init_method == 'optimised':
             initial_state = optimized_start(self.problem.forward_model, self.problem.true_observation, self.problem.n_dim, self.n_chains, steps=3000)
         else:
             raise ValueError('Initialisation method {} not recognised'.format(self.init_method))
 
+
+        print('Initial state vs true params: {}'.format(list(zip(initial_state[0, :].numpy(), self.problem.true_params))))
         print('Ready to go - beginning sampling at {}'.format(datetime.datetime.now().ctime()))
         start_time = datetime.datetime.now()
         samples, is_accepted = hmc(
@@ -43,20 +44,27 @@ class SamplerHMC(Sampler):
             num_results=self.n_samples,
             num_burnin_steps=self.n_burnin
         )
+        samples = samples.numpy()
+        is_accepted = is_accepted.numpy()
 
-        flat_samples = samples.numpy().reshape(-1, 7)
+        # do not flatten, instead keep chains for later diagnostics
+        # flat_samples = samples.numpy().reshape(-1, 7)
 
         # mask rows with values outside of bounds
         # do not simply cut from array as consistent shapes are required by hdf5 virtual dataset
-        within_bounds = (np.max(flat_samples, axis=1) < 1.) & (np.min(flat_samples, axis=1) > 0.)
-        flat_samples[within_bounds] = np.nan  # access by slice, okay?
-        masked_samples = np.ma.masked_invalid(flat_samples)
+
+        # within bounds is true for all params (dim 2) where any param is out of bounds
+        # all_params_within_bounds = (np.max(samples, axis=2) < 1.) & (np.min(samples, axis=2) > 0.)
+        # within_bounds = np.stack([all_params_within_bounds for _ in range(samples.shape[2])], axis=2)
+        # np.copyto(samples, np.nan, where=~within_bounds)  # inplace 
+        # masked_samples = np.ma.masked_invalid(samples)
+        masked_samples = samples  # temp
         end_time = datetime.datetime.now()
         elapsed = end_time - start_time
         ms_per_sample = 1000 * elapsed.total_seconds() / (self.n_samples * self.n_chains)  # not counting burn-in as a sample, so really quicker
         print('Sampling {} x {} chains complete in {}, {} ms per sample'.format(self.n_samples, self.n_chains, elapsed, ms_per_sample))
         
-        print('Acceptance ratio: {:.4f}'.format(is_accepted.numpy()))
+        print('Acceptance ratio: {:.4f}'.format(is_accepted))
         if is_accepted < 0.01:
             logging.critical('HMC failed to adapt - is step size too small? Is there some burn-in?')
         elif is_accepted < 0.3:
@@ -65,7 +73,7 @@ class SamplerHMC(Sampler):
         print('True vs. median recovered parameters: ', list(zip(self.problem.true_params, np.ma.median(masked_samples, axis=0))))
         return masked_samples
 
-
+# don't decorate
 def hmc(log_prob_fn, initial_state, num_results=int(10e3), num_burnin_steps=int(1e3)):
 
     # Initialize the HMC transition kernel.
@@ -76,7 +84,6 @@ def hmc(log_prob_fn, initial_state, num_results=int(10e3), num_burnin_steps=int(
             step_size=2,
             state_gradients_are_stopped=True),
         num_adaptation_steps=int(num_burnin_steps * 0.8))
-    assert tf.executing_eagerly()
 
     @tf.function
     def run_chain():
@@ -96,27 +103,27 @@ def hmc(log_prob_fn, initial_state, num_results=int(10e3), num_burnin_steps=int(
     return samples, is_accepted
 
 
-def find_best_params(x, forward_model, observation, steps, optimizer=tf.train.AdamOptimizer(learning_rate=1e-2)):
-    log_prob_fn = get_log_prob_fn(forward_model, observation, batch_dim=tf.shape(x)[0])
+def find_best_params(forward_model, observation, param_dim, n_chains, steps, optimizer=tf.keras.optimizers.Adam()):
+    log_prob_fn = get_log_prob_fn(forward_model, observation, batch_dim=1)  # for now
+    params = tf.Variable(np.ones((1, param_dim), dtype=np.float32) * 0.5, dtype=tf.float32)
     for _ in range(steps):
         with tf.GradientTape() as tape:
-            loss_value = -log_prob_fn(x)  # a very important minus sign...
-            grads = tape.gradient(loss_value, [x])[0]
-            grads_and_vars = [(grads, x)]
+            loss_value = -log_prob_fn(params)  # a very important minus sign...
+            grads = tape.gradient(loss_value, [params])[0]
+            grads_and_vars = [(grads, params)]
             optimizer.apply_gradients(grads_and_vars)
-    return x
+    return params
 
 
 def optimized_start(forward_model, observation, param_dim, n_chains, steps=1000):
-    params = tf.Variable(np.ones((1, param_dim), dtype=np.float32) * 0.5, dtype=tf.float32)
-    best_params = find_best_params(forward_model, params, observation, steps)
+    best_params = find_best_params(forward_model, observation, param_dim, n_chains, steps)
     initial_state = tf.reshape(tf.stack([best_params for n in range(n_chains)]), (n_chains, param_dim))
     return initial_state
 
 
-def many_random_starts(forward_model, true_observation, true_params, n_chains):
+def many_random_starts(forward_model, true_observation, param_dim, n_chains):
     overproposal_factor = 10
-    overproposed_initial_state = tf.random.uniform(shape=(n_chains * overproposal_factor, len(true_params)))
+    overproposed_initial_state = tf.random.uniform(shape=(n_chains * overproposal_factor, param_dim))
     log_prob_fn = get_log_prob_fn(forward_model, true_observation, batch_dim=n_chains * overproposal_factor)
     start_time = datetime.datetime.now()
     initial_log_probs = log_prob_fn(overproposed_initial_state)

agnfinder/tf_sampling/main.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     See README.md in this folder for more.
 
     Example use:
-    /data/miniconda3/envs/agnfinder/bin/python agnfinder/tf_sampling/main.py --checkpoint-loc results/checkpoints/latest_tf --test-json data/lfi_test_case.json --n-chains 32 --n-samples 1000 --n-burnin 1500
+    python agnfinder/tf_sampling/main.py --checkpoint-loc results/checkpoints/latest --test-json data/lfi_test_case.json --n-chains 32 --n-samples 1000 --n-burnin 1500
 
     """
     tf.enable_eager_execution()
@@ -85,10 +85,10 @@ if __name__ == '__main__':
     
     init_method = 'random'
     sampler = SamplerHMC(problem, n_burnin, n_samples, n_chains, init_method=init_method)
-    flat_samples = sampler()
+    samples = sampler()
 
     labels = ['mass', 'dust2', 'tage', 'tau', 'agn_disk_scaling', 'agn_eb_v', 'agn_torus_scaling']
-    figure = corner.corner(flat_samples, labels=labels)  # middle dim is per chain
+    figure = corner.corner(samples.reshape(-1, 7), labels=labels)  # middle dim is per chain
     figure.savefig('results/samples_{}_then_{}x{}.png'.format(n_burnin, n_samples, n_chains))
 
     exit()  # avoids weird tf.function error

agnfinder/tf_sampling/run_sampler.py
@@ -1,7 +1,9 @@
 import os
 import logging
 import argparse
+import json  # temporary, for debugging
 
+from tqdm import tqdm
 import numpy as np
 import h5py
 import tensorflow as tf  # just for eager toggle
@@ -20,38 +22,43 @@ def record_performance_on_galaxies(checkpoint_loc, n_galaxies_to_check, n_burnin
     if not os.path.isdir(save_dir):
         os.mkdir(save_dir)
 
-    for i in range(n_galaxies_to_check):
+    for i in tqdm(range(n_galaxies_to_check)):
         record_performance(x_test, i, y_test, emulator, n_burnin, n_samples, n_chains, init_method, save_dir)
 
 
 def record_performance(x_test, i, y_test, emulator, n_burnin, n_samples, n_chains, init_method, save_dir):
 
-    logging.warning('Overriding actual params with fixed test case!')
-    # true_params = x_test[i]
-    # true_observation = y_test[i]
-    with open('data/lfi_test_case.json', 'r') as f:
-        test_pair = json.load(f)
-        true_params = np.array(test_pair['true_params']).astype(np.float32)
-        true_observation = np.array(test_pair['true_observation']).astype(np.float32)
+    # if i == 0:  # for now, always sample this galaxy as a test case
+    #     logging.warning('Overriding actual params with fixed test case!')
+    #     with open('data/lfi_test_case.json', 'r') as f:
+    #         test_pair = json.load(f)
+    #         true_params = np.array(test_pair['true_params']).astype(np.float32)
+    #         true_observation = np.array(test_pair['true_observation']).astype(np.float32)
+    # else:
+    true_params = x_test[i]
+    true_observation = -y_test[i]
+
+    logging.warning('True params: {}'.format(true_params))
+    logging.warning('True observation: {}'.format(true_observation))
 
     problem = api.SamplingProblem(true_observation, true_params, forward_model=emulator)
     sampler = hmc.SamplerHMC(problem, n_burnin, n_samples, n_chains, init_method=init_method)
-    flat_samples = sampler()
+    samples = sampler()
 
     # explicitly remove old files to avoid shape mismatch issues
     save_file = os.path.join(save_dir, 'galaxy_{}_performance.h5'.format(i))
     if os.path.isfile(save_file):
         os.remove(save_file)
 
-    expected_shape = (n_samples * n_chains, 7)
-    if flat_samples.shape != expected_shape:
+    expected_shape = (n_samples, n_chains, 7)
+    if samples.shape != expected_shape:
         logging.warning('Samples not required shape - skipping save to avoid virtual dataset issues')
-        logging.warning('actual {} vs expected {}'.format(flat_samples.shape, expected_shape))
+        logging.warning('actual {} vs expected {}'.format(samples.shape, expected_shape))
     else:
         
         f = h5py.File(save_file, mode='w')
-        logging.warning('shape of samples: {}'.format(flat_samples.shape))
-        f.create_dataset('samples', data=flat_samples)
+        logging.warning('shape of samples: {}'.format(samples.shape))
+        f.create_dataset('samples', data=samples)
         f.create_dataset('true_params', data=true_params)
         f.create_dataset('true_observations', data=true_observation)
 
@@ -62,14 +69,14 @@ def aggregate_performance(save_dir, n_samples, n_chains):
     n_sources = len(performance_files)
     logging.warning('Using source files: {}'.format(performance_files))
     logging.debug('Specifing expected data')
-    samples_vl = h5py.VirtualLayout(shape=(n_sources, n_chains * n_samples, 7), dtype='f')
+    samples_vl = h5py.VirtualLayout(shape=(n_sources, n_samples, n_chains, 7), dtype='f')
     true_params_vl = h5py.VirtualLayout(shape=(n_sources, 7), dtype='f')
     true_observations_vl = h5py.VirtualLayout(shape=(n_sources, 12), dtype='f')
 
     logging.debug('Specifying sources')
     for i, file_loc in enumerate(performance_files):
         assert os.path.isfile(file_loc)
-        samples_source_shape = (n_chains * n_samples, 7)
+        samples_source_shape = (n_samples, n_chains, 7)
         logging.warning('shape of samples expected: {}'.format(samples_source_shape))
         samples_vl[i] = h5py.VirtualSource(file_loc, 'samples', shape=samples_source_shape)
         true_params_vl[i] = h5py.VirtualSource(file_loc, 'true_params', shape=(7,))
@@ -107,7 +114,7 @@ if __name__ == '__main__':
     Evaluating performance at recovering posteriors can be done in `evaluate_performance.py`
 
     Example use: 
-    /data/miniconda3/envs/agnfinder/bin/python /Data/repos/agnfinder/agnfinder/tf_sampling/run_sampler.py --checkpoint-loc results/checkpoints/latest_tf --output-dir results/emulated_sampling
+    python agnfinder/tf_sampling/run_sampler.py --checkpoint-loc results/checkpoints/latest --output-dir results/emulated_sampling
 
     """
     parser = argparse.ArgumentParser(description='Run emulated HMC on many galaxies')
@@ -124,26 +131,16 @@ if __name__ == '__main__':
     
     logging.getLogger().setLevel(logging.WARNING)  # some third party library is mistakenly setting the logging somewhere...
 
-<<<<<<< HEAD
-    checkpoint_loc = 'results/checkpoints/weights_only/latest_tf'  # must match saved checkpoint of emulator
-    n_galaxies = 2
-    n_burnin = 2000
-    n_samples = 1000
-    n_chains = 32
-    init_method = 'random'
-    # init_method = 'roughly_correct'
-    # init_method = 'optimised'
-    save_dir = 'results/recovery/latest_{}_{}_{}'.format(n_galaxies, n_samples * n_chains, init_method)
-=======
     checkpoint_loc =  args.checkpoint_loc
+    output_dir = args.output_dir
+    assert checkpoint_loc is not None
+    assert output_dir is not None
     n_galaxies = args.n_galaxies
     n_burnin = args.n_burnin
     n_samples = args.n_samples
     n_chains = args.n_chains
-    output_dir = args.output_dir
     init_method = args.init_method
-    save_dir = os.path.join(output_dir, 'latest_{}_{}_{}'.format(n_galaxies, n_samples * n_chains, init_method))
->>>>>>> run-from-scratch
+    save_dir = os.path.join(output_dir, 'latest_{}_{}_{}_{}'.format(n_galaxies, n_samples, n_chains, init_method))
 
     record_performance_on_galaxies(checkpoint_loc, n_galaxies, n_burnin, n_samples, n_chains, init_method, save_dir)
     aggregate_performance(save_dir, n_samples, n_chains)