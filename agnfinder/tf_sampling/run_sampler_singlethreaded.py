8a257057c5630cb8f0a2432432f76d8d3f692c4c
Mike Walmsley <walmsleymk1@gmail.com>
2020-01-21 18:12:50 +0000

    Run again for extra galaxies

--- agnfinder/tf_sampling/run_sampler_singlethreaded.py
+++ agnfinder/tf_sampling/run_sampler_singlethreaded.py
@@ -57,8 +57,8 @@ def record_performance_on_galaxies(checkpoint_loc, selected_catalog_loc, max_gal
         _, _, x_test, y_test = deep_emulator.data(cube_dir='data/cubes/latest')  # TODO could make as arg
         x_test = x_test.astype(np.float32)
         y_test = y_test.astype(np.float32)
-        # galaxy_indices = get_galaxies_without_results(n_chains)  # commenting out for now
-        galaxy_indices = np.arange(n_chains)
+        galaxy_indices = get_galaxies_without_results(n_chains)  # commenting out for now
+        # galaxy_indices = np.arange(n_chains)
         true_params = x_test[galaxy_indices, 1:]  # excluding the 0th redshift param, which we treat as fixed
         redshifts = x_test[galaxy_indices, :1].astype(np.float32)  # shape (n_galaxies, 1)
         true_observation = deep_emulator.denormalise_photometry(y_test[galaxy_indices]) 
