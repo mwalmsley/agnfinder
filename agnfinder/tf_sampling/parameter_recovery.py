16f9508249083555f550f4527d677bec286887a7
Mike Walmsley <walmsleymk1@gmail.com>
2020-01-22 10:26:13 +0000

    Font scale to 2

--- agnfinder/tf_sampling/parameter_recovery.py
+++ agnfinder/tf_sampling/parameter_recovery.py
@@ -94,7 +94,7 @@ def get_cmap(hue_val):
 if __name__ == '__main__':
 
     sns.set_context('notebook')
-    sns.set(font_scale=1.)
+    sns.set(font_scale=2.)
 
     parser = argparse.ArgumentParser(description='Find AGN!')
     parser.add_argument('--save-dir', dest='save_dir', type=str)
