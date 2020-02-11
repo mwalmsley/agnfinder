f477929c1a2e3193e9df66046c1abdd83092ec8d
Mike Walmsley <walsleymk1@gmail.com>
2020-02-11 15:11:37 +0000

    Run another 512, this time from saved file

--- simulate_in_parallel.sh
+++ simulate_in_parallel.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-CUBES=6  # zeus cores
-SAMPLES_PER_CUBE=50000  # 14 mins for 100k, 140 mins for 1m, etc.
-ZMAX=4.0
+CUBES=20  # zeus cores
+SAMPLES_PER_CUBE=1000000  # 14 mins for 100k, 140 mins for 1m, etc.
+ZMAX=0.5  # okay to change this, but never change the 4.0 in simulate_samples.py
 
 ZSTEP=$(echo "scale=4;$ZMAX/$CUBES" | bc)
 
