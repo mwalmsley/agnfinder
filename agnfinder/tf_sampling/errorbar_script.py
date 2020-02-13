3c8504683661c66ca60443e812481e104b68bb42
Mike Walmsley <walsleymk1@gmail.com>
2020-02-13 17:29:09 +0000

    Add contour script

---
+++ agnfinder/tf_sampling/errorbar_script.py
@@ -0,0 +1,76 @@
+import os
+import argparse
+
+import h5py
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns
+sns.set_context('notebook')
+import corner
+from tqdm.auto import tqdm  # notebook friendly
+import glob
+import arviz as az
+
+from agnfinder.prospector.main import load_catalog
+from agnfinder.prospector import load_photometry
+from agnfinder import simulation_samples, simulation_utils
+from agnfinder.tf_sampling import parameter_recovery, percentile_limits
+
+
+def get_hpd(x, ci=0.8):
+    if len(x) == 0:
+        return np.array([np.nan, np.nan])
+    return az.hpd(x[~np.isnan(x)], credible_interval=ci)
+
+
+if __name__ == '__main__':
+
+    sns.set_context('notebook')
+    sns.set(font_scale=1.3)
+
+    parser = argparse.ArgumentParser(description='Find AGN!')
+    parser.add_argument('--save-dir', dest='save_dir', type=str)
+    args = parser.parse_args()
+
+    params, marginals, true_params, samples = parameter_recovery.load_samples(args.save_dir, min_acceptance=0.6, max_redshift=4.0)
+
+    dummy_array = np.zeros(42)  # anything
+    n_param_bins = 10
+    _, param_bins = np.histogram(dummy_array, range=(0., 1.), bins=n_param_bins)
+    bin_centers = param_bins[1:] + (param_bins[0:-1] - param_bins[1:]) / 2
+
+    samples_by_truth = [[] for n in range(len(param_bins))]
+
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(12, 12))
+    all_axes = [ax for row in axes for ax in row]
+
+    for which_param in range(8):
+
+        ax = all_axes[n]
+
+        samples_by_truth = [[] for n in range(len(bin_centers))]
+
+        for galaxy_n in range(len(samples)):
+            true_param = true_params[galaxy_n, which_param]
+            true_param_index = np.digitize(true_param, param_bins) - 1
+        #     print(true_param_index)
+            samples_by_truth[true_param_index].append(np.squeeze(samples[galaxy_n, :, which_param]))
+        # samples_by_truth = np.array(samples_by_truth)
+
+        for n in range(len(samples_by_truth)):
+            samples_by_truth[n] = np.array(samples_by_truth[n]).flatten()
+
+        bounds_by_truth = np.array([get_hpd(x) for x in samples_by_truth])
+        medians = np.array([np.median(x) for x in samples_by_truth])
+
+        delta_bounds_by_truth = bounds_by_truth.transpose()
+        delta_bounds_by_truth[1, :] = delta_bounds_by_truth[1, :] - medians
+        delta_bounds_by_truth[0, :] = medians - delta_bounds_by_truth[0, :]
+
+        ax.fill_between(bin_centers, bounds_by_truth[:, 0], bounds_by_truth[:, 1], alpha=0.5)
+        ax.plot(bin_centers, bin_centers, linestyle='--', color='k')
+
+    fig.tight_layout()
+    fig.savefig('results/latest_contours.png')
--- agnfinder/tf_sampling/parameter_recovery.py
+++ agnfinder/tf_sampling/parameter_recovery.py
@@ -11,57 +11,59 @@ import h5py
 from tqdm import tqdm
 
 
+def main(save_dir, min_acceptance, max_redshift):
+    params, marginals, true_params, _ = load_samples(save_dir, min_acceptance, max_redshift)  # don#t need samples themselves
+    posterior_records, param_bins = get_all_posterior_records(marginals, true_params, n_param_bins=50, n_posterior_bins=50)
+    fig, axes = plot_posterior_stripes(posterior_records, param_bins, params)
+    return fig, axes
 
-def plot_posterior_stripes(params, marginals, true_params, n_param_bins=50, n_posterior_bins=50):
-    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(12, 12))
-    all_axes = [ax for col in axes for ax in col]
-    sns.set_context('notebook')
-    sns.set_style('white')
 
+def get_all_posterior_records(marginals, true_params, n_param_bins, n_posterior_bins):
     # with a made up array, get the bins to use
     dummy_array = np.zeros(42)  # anything
     _, param_bins = np.histogram(dummy_array, range=(0., 1.), bins=n_param_bins)
 
-    for param_n in range(len(params)):
+    posterior_records = []
+    for param_n in range(true_params.shape[1]):
+        posterior_record = get_posterior_record(marginals, true_params, param_n, param_bins, n_param_bins, n_posterior_bins)
+        posterior_records.append(posterior_record)
+    return posterior_records, param_bins
+
+
+def get_posterior_record(marginals, true_params, param_n, param_bins, n_param_bins, n_posterior_bins):
+    galaxy_counts = np.zeros((n_param_bins))  # to track how many galaxies have been added
+    posterior_record = np.zeros((n_param_bins, n_posterior_bins)) * 0
+    for galaxy_n, _ in enumerate(marginals):
+        true_param = true_params[galaxy_n, param_n]
+        true_param_index = np.digitize(true_param, param_bins)  # fnd the bin index for true_param
+
+        stripe = marginals[galaxy_n, param_n]
+        if true_param_index < n_param_bins:  # exclude =50 edge case TODO
+            posterior_record[true_param_index] += np.nan_to_num(stripe)  # nans to 0's
+
+            galaxy_counts[true_param_index] += 1
+
+    # divide out by how many galaxies were added at each index
+    # posterior_record = posterior_record / galaxy_counts
+    for n in range(len(galaxy_counts)):
+        posterior_record[n] = posterior_record[n] / galaxy_counts[n]
+    # print(posterior_record)
+    # replace any 0's with nans, for clarity
+    posterior_record[np.isclose(posterior_record, 0)] = np.nan
+    # trim extreme values
+    ceiling = np.quantile(posterior_record[~np.isnan(posterior_record)], .95)
+    posterior_record = np.clip(posterior_record, 0, ceiling)
+    return posterior_record
+
+
+def plot_posterior_stripes(posterior_records, param_bins, params):
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(12, 12))
+    all_axes = [ax for col in axes for ax in col]
+    sns.set_context('notebook')
+    sns.set_style('white')
+    for param_n, record in enumerate(posterior_records):
         ax = all_axes[param_n]
-        posterior_record = np.zeros((n_param_bins, n_posterior_bins)) * 0
-        posterior_colors = np.zeros((n_param_bins, n_posterior_bins, 4)) * 0
-        galaxy_counts = np.zeros((n_param_bins))  # to track how many galaxies have been added
-        for galaxy_n, _ in enumerate(marginals):
-            true_param = true_params[galaxy_n, param_n]
-            true_param_index = np.digitize(true_param, param_bins)  # fnd the bin index for true_param
-            # print(true_param_index)
-            stripe = marginals[galaxy_n, param_n]
-            if true_param_index < n_param_bins:  # exclude =50 edge case TODO
-                posterior_record[true_param_index] += np.nan_to_num(stripe)  # nans to 0's
-                # posterior_record[true_param_index] = stripe
-                galaxy_counts[true_param_index] += 1
-
-        # print(posterior_record[:, 0])
-        # print(posterior_record)
-        # divide out by how many galaxies were added at each index
-        # posterior_record = posterior_record / galaxy_counts
-        for n in range(len(galaxy_counts)):
-            posterior_record[n] = posterior_record[n] / galaxy_counts[n]
-        # print(posterior_record)
-        # replace any 0's with nans, for clarity
-        posterior_record[np.isclose(posterior_record, 0)] = np.nan
-        # trim extreme values
-        ceiling = np.quantile(posterior_record[~np.isnan(posterior_record)], .95)
-        posterior_record = np.clip(posterior_record, 0, ceiling)
-
-        # plot in single color
-        ax.pcolormesh(param_bins, param_bins, np.transpose(posterior_record), cmap='Blues')  
-        # OR plot colored by value of one param
-        # coloring_param_index = 5
-        # for galaxy_n, galaxy in enumerate(marginals):
-        #     custom_cmap = get_cmap(true_params[galaxy_n, coloring_param_index])
-        #     true_param = true_params[galaxy_n, param_n]
-        #     true_param_index = np.digitize(true_param, param_bins)  # find the bin index for true_param
-        #     if true_param_index < n_param_bins:  # exclude =50 edge case TODO
-        #         posterior_colors[true_param_index] = custom_cmap(posterior_record[true_param_index])
-        # ax.imshow(np.transpose(posterior_colors, axes=[1, 0, 2]), origin='lower')
-        
+        ax.pcolormesh(param_bins, param_bins, np.transpose(record), cmap='Blues')  
         ax.grid(False)
         # ax.plot([0., 50.], [0., 50.], 'k--', alpha=0.3)
         ax.set_title('{}'.format(params[param_n]), fontsize=22)
@@ -99,6 +101,7 @@ def load_samples(save_dir, min_acceptance, max_redshift):
         # don't care about fixed params
     params = rename_params(params)
 
+    all_samples = []
     marginals = np.zeros((len(galaxy_locs), len(params), 50))  # TODO magic number which must match run_sampler.py
     true_params = np.zeros((len(galaxy_locs), len(params)))
     allowed_redshift = np.zeros(len(galaxy_locs), dtype=bool)
@@ -115,7 +118,9 @@ def load_samples(save_dir, min_acceptance, max_redshift):
         # print(num_geq_80p, num_geq_80p.shape)
         allowed_acceptance[n] = np.mean(num_geq_80p) > min_acceptance
         samples = np.squeeze(f['samples'][...]) # okay to load, will not keep
+        all_samples.append(samples[::25])
         successful_run[n] = within_percentile_limits(samples)
+        
         # successful_run[n] = True  # disable for now
         if 'Redshift' not in params:
             allowed_redshift[n] = f['fixed_params'][0] * 4 < max_redshift  # absolutely must match hypercube physical redshift limit
@@ -130,9 +135,10 @@ def load_samples(save_dir, min_acceptance, max_redshift):
     logging.info('{} galaxies of {} have redshift > {}'.format(allowed_redshift.sum(), len(allowed_redshift), max_redshift))
     logging.warning('{} galaxies of {} are successful'.format(successful_run.sum(), len(successful_run)))
     accept = allowed_acceptance & allowed_redshift & successful_run
+    all_samples = np.array(all_samples)[accept]
     marginals = marginals[accept]
     true_params = true_params[accept]
-    return params, marginals, true_params
+    return params, marginals, true_params, all_samples
 
 
 def percentile_spreads(samples, quantile_width=50):
@@ -161,17 +167,13 @@ def within_percentile_limits(samples, limits=None):
     # good_agn_torus= pcs_10[6] > 0.02
     # good_inclination = pcs_10[7] > 0.02
 
-    return ~(good_agn_extinction & good_tau)
+    return good_agn_extinction & good_tau
+    # return ~(good_agn_extinction & good_tau)  # flip if you like, to debug
 
     # return compare_percentiles_with_limits(valid_pcs, limits)
 
 def compare_percentiles_with_limits(valid_pcs, limits):
     return np.squeeze(np.sum(valid_pcs < limits, axis=1) < 2.)  # no more than 1 parameter can have less 75%-25% spread than the limits (set to discard 15% of data)
-
-def main(save_dir, min_acceptance, max_redshift):
-    params, marginals, true_params = load_samples(save_dir, min_acceptance, max_redshift)
-    fig, axes = plot_posterior_stripes(params, marginals, true_params)
-    return fig, axes
 
 
 if __name__ == '__main__':
