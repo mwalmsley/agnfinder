63ef07b4f0c6c2832402ed19b2c16150c95a8bfe
Mike Walmsley <walsleymk1@gmail.com>
2020-02-13 17:57:36 +0000

    Add ci arg

--- agnfinder/tf_sampling/errorbar_script.py
+++ agnfinder/tf_sampling/errorbar_script.py
@@ -19,7 +19,7 @@ from agnfinder import simulation_samples, simulation_utils
 from agnfinder.tf_sampling import parameter_recovery, percentile_limits
 
 
-def get_hpd(x, ci=0.8):
+def get_hpd(x, ci):
     if len(x) == 0:
         return np.array([np.nan, np.nan])
     return az.hpd(x[~np.isnan(x)], credible_interval=ci)
@@ -32,6 +32,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Find AGN!')
     parser.add_argument('--save-dir', dest='save_dir', type=str)
+    parser.add_argument('--ci', dest='ci', type=float, default=0.9)
     args = parser.parse_args()
 
     params, marginals, true_params, samples = parameter_recovery.load_samples(args.save_dir, min_acceptance=0.6, max_redshift=4.0)
@@ -55,24 +56,23 @@ if __name__ == '__main__':
         for galaxy_n in range(len(samples)):
             true_param = true_params[galaxy_n, which_param]
             true_param_index = np.digitize(true_param, param_bins) - 1
-        #     print(true_param_index)
             samples_by_truth[true_param_index].append(np.squeeze(samples[galaxy_n, :, which_param]))
-        # samples_by_truth = np.array(samples_by_truth)
 
         for n in range(len(samples_by_truth)):
             samples_by_truth[n] = np.array(samples_by_truth[n]).flatten()
 
-        bounds_by_truth = np.array([get_hpd(x) for x in samples_by_truth])
+        bounds_by_truth = np.array([get_hpd(x, args.ci) for x in samples_by_truth])
         medians = np.array([np.median(x) for x in samples_by_truth])
 
         # delta_bounds_by_truth = bounds_by_truth.copy().transpose()
         # delta_bounds_by_truth[1, :] = delta_bounds_by_truth[1, :] - medians
         # delta_bounds_by_truth[0, :] = medians - delta_bounds_by_truth[0, :]
 
-        ax.fill_between(bin_centers, bounds_by_truth[:, 0], bounds_by_truth[:, 1], alpha=0.5)
+        ax.fill_between(bin_centers, bounds_by_truth[:, 0], bounds_by_truth[:, 1], alpha=0.5, label='NN-HMC')
         ax.plot(bin_centers, bin_centers, linestyle='--', color='k')
         ax.set_xlabel('Truth')
-        ax.set_ylabel(r'80% credible interval')
+        ax.set_ylabel(r'90% credible interval')  # change manually
+        ax.legend()
 
     fig.tight_layout()
     fig.savefig('results/latest_contours.png')
